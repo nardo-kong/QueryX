4b8bfa4424bad297d173b8bc681851d358371eaa
Nardo <nardokong@gmail.com>
2025-05-12 16:32:44 +0800

    feature: Connection Status Bar enhance

---
+++ QueryX/Converters/StartsWithParameterConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace QueryX.Converters
+{
+    public class StartsWithParameterConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string? actualValue = value as string;
+            string? param = parameter as string;
+
+            if (actualValue != null && param != null)
+            {
+                return actualValue.StartsWith(param, StringComparison.OrdinalIgnoreCase);
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
