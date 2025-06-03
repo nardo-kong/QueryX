19f43cbfdea415ca706523a96d6b10adac6bf23e
Nardo <nardokong@gmail.com>
2025-06-03 17:50:04 +0800

    BUG: Fixing Parameters DataType ComboBox

---
+++ QueryX/Helpers/EnumItemsSource.cs
@@ -0,0 +1,12 @@
+﻿using QueryX.Models; // For ParameterDataType
+using System;
+using System.Linq;
+
+namespace QueryX.Helpers
+{
+    public static class EnumItemsSource
+    {
+        public static ParameterDataType[] ParameterDataTypes { get; } =
+            Enum.GetValues(typeof(ParameterDataType)).Cast<ParameterDataType>().ToArray();
+    }
+}
